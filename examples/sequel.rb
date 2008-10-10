0a4912e25deddd3db0baac5d3a3279b3a85cf7a8
Lori Holden <email+git@loriholden.com>
2008-10-10 00:26:45 -0700

    Big revamp of my slides

--- examples/sequel.rb
+++ examples/sequel.rb
@@ -5,7 +5,7 @@ DB['select * from posts'].each {|row| p row }
 posts = DB[:posts]
 
 # Restrict results using a filter
-filtered = posts.filter :category => ['ruby', 'postgres']
+filtered = posts.filter(:category => ['ruby', 'postgres'])
 
 # Post Model
 class Post < Sequel::Model
@@ -18,3 +18,23 @@ end
 
 # Find post with title 'hello world'
 post = Post[:title => 'hello world']
+
+# Add an index
+DB.add_index :posts, [:author, :title], :unique => true
+
+# Insert a new record
+DB[:posts] << {:title => 'Awesome post', :body => 'LOL'}
+
+# Union selects
+DB[:items].union(DB[:other_items]).sql
+#=> "SELECT * FROM items UNION SELECT * FROM other_items"
+
+# Its like... SQL or something ;)
+DB[:items].
+  select(:x, :y, :z).
+  filter((:x > 1) & (:y > 2)).
+  order(:z.desc).
+  sql
+  
+#=> "SELECT `x`, `y`, `z` FROM `items` 
+#     WHERE ((`x` > 1) AND (`y` > 2)) ORDER BY `z` DESC"
